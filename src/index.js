e5a490e9ea1ac792f22bc6c1381b4d3a8774b072
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2017-02-10 05:26:08 -0500

    Sequelize changes. Added comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import { render } from 'react-dom';
 import routes from './app/routes';
+// Added something
 
 render (
 	<Router history={browserHistory} routes={routes} />,
