c0c047a00d9f2184cc520f1e7e867aeadcdf08a7
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2017-02-19 10:00:36 -0500

    Establish db connection

--- server/srcServer.js
+++ server/srcServer.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import webpack from 'webpack';
+import mongoose from 'mongoose';
+import mongoconfig from '../src/api/config/dbconfig';
 import config from '../webpack.config.dev';
 
 const app = express();
@@ -12,7 +14,11 @@ app.use(require('webpack-dev-middleware')(compiler, {
 	publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler))
+app.use(require('webpack-hot-middleware')(compiler));
+
+mongoose.connect(mongoconfig.db, (err) => {
+  err ? console.log('Error connecting to the database') : console.log('Connected to database');
+})
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
---
+++ src/api/config/dbconfig.js
@@ -0,0 +1,5 @@
+const mongoconfig = {
+	db : 'mongodb://sheshe:sheshe@ds157539.mlab.com:57539/mentorher'
+};
+
+export default mongoconfig;
